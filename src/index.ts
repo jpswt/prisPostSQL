173fdcf9f31714df4bf5477429341c7043e7ab01
jpswt <joel_swt@yahoo.com>
2023-07-23 23:01:04 -0500

    fix delete

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,14 @@ import { Query, Mutation, Profile, Post, User } from './resolvers';
 import { PrismaClient, Prisma } from '@prisma/client';
 import { DefaultArgs } from '@prisma/client/runtime';
 import { getUserFromToken } from './utils/getUserFromToken';
+import express from 'express';
+import cors from 'cors';
+import { expressMiddleware as apolloMiddleware } from '@apollo/server/express4';
 
+const PORT = 4000;
+
+const app = express();
+app.use(cors(), express.json());
 export interface Context {
 	prisma: PrismaClient;
 	userInfo: {
@@ -16,7 +23,17 @@ export interface Context {
 
 export const prisma = new PrismaClient();
 
-const server = new ApolloServer({
+// const server = new ApolloServer({
+// 	typeDefs,
+// 	resolvers: {
+// 		Query,
+// 		Mutation,
+// 		Profile,
+// 		Post,
+// 		User,
+// 	},
+// });
+const apolloServer = new ApolloServer({
 	typeDefs,
 	resolvers: {
 		Query,
@@ -27,14 +44,37 @@ const server = new ApolloServer({
 	},
 });
 
-const { url } = await startStandaloneServer(server, {
-	context: async ({ req }: any): Promise<Context> => {
-		const userInfo = await getUserFromToken(req.headers.authorization);
-		return {
-			prisma,
-			userInfo,
-		};
-	},
+await apolloServer.start();
+app.use(
+	'/graphql',
+	apolloMiddleware(apolloServer, {
+		context: async ({ req }: any): Promise<Context> => {
+			const userInfo = await getUserFromToken(req.headers.authorization);
+			return {
+				prisma,
+				userInfo,
+			};
+		},
+	})
+);
+
+app.use('/', (req, res) => {
+	res.json('welcome to my server');
 });
 
-console.log(`🚀 Server ready at ${url}graphql`);
+app.listen({ port: PORT }, () => {
+	console.log(`Server running on port ${PORT}`);
+	console.log(`Graphql endpoint is http://localhost:${PORT}/graphql`);
+});
+
+// const { url } = await startStandaloneServer(server, {
+// 	context: async ({ req }: any): Promise<Context> => {
+// 		const userInfo = await getUserFromToken(req.headers.authorization);
+// 		return {
+// 			prisma,
+// 			userInfo,
+// 		};
+// 	},
+// });
+
+// console.log(`🚀 Server ready at ${url}graphql`);
